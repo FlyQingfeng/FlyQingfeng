86670fe66c7b8430c9dc7b11d4b1caadeaccb016
qingfeng <2758534648@qq.com>
2022-09-10 22:29:59 +0800

    也不知道更新了什么鬼

--- js/learn.js
+++ js/learn.js
@@ -1,3 +1,4 @@
+
 //弹窗样式
 iziToast.settings({
     timeout: 10000,
@@ -86,3 +87,118 @@ setTimeout(function () {
 }, 3000);
 
 
+//屏蔽右键
+document.oncontextmenu = function () {
+    iziToast.show({
+        timeout: 2000,
+        icon: "fa-solid fa-circle-exclamation",
+        message: '为了浏览体验，本站禁用右键'
+    });
+    return false;
+}
+
+//设置默认页面
+$(".list ul").eq(0).show().siblings().hide();
+$('#top_menu_btn').hide();//隐藏顶部按钮
+
+//监听网页宽度
+let is_sm = false;
+window.addEventListener('load', function () {
+    window.addEventListener('resize', function () {
+        //获取网页宽度
+        webWidth = window.innerWidth;
+        //关闭移动端样式
+        if (window.innerWidth >= 600) {
+            $('#left_menu_p').css({
+                "width": "25%"
+            });
+            $('#name_text').show();
+            $('#top_menu_btn').hide();
+            is_sm = false;
+        } //启用移动端样式
+        if (window.innerWidth <= 990) {
+            $('#top_menu_btn').show();
+            is_sm = true;
+            $('#left_menu_p').css({
+                "width": "0%"
+            });
+            $('#name_text').hide();
+
+        }
+    })
+})
+
+//移动端菜单打开
+function left_menu_open() {
+    $('#left_menu_p').css({
+        "width": "100%"
+    });
+    $('#name_text').show();
+}
+//移动端菜单关闭
+function left_menu_off() {
+    $('#left_menu_p').css({
+        "width": "0%"
+    });
+    $('#name_text').hide();
+}
+function getFile(path) {
+    var temp_str = "null";
+    var reader = path;
+    reader.readAsText(this.files[0]);
+    reader.onload = function () {
+        if (reader.result) {
+            temp_str = reader.result;
+        }
+    };
+    console.log(temp_str);
+    return temp_str;
+}
+function updateWeb() {
+    //目前还不会做动态更新列表
+    
+    // let count = document.getElementById('#right_main').childElementCount;
+    // let right_main_uls = document.getElementById('#right_main').children;
+    // for (let i = 0; i < count; i++) {
+
+    // }
+}
+
+$(function () {
+    let is_left_menu_show = false;
+    // 页面切换
+    $('.list_bt .bt').click(function () {
+        $(this).addClass('active').siblings().removeClass('active');
+        $(".main_list ul").eq($(this).index()).show().siblings().hide();
+        if (is_sm) {
+            left_menu_off();
+            is_left_menu_show = false;
+        }
+    })
+    //分享内容
+    $('.list_bt .write').click(function () {
+        iziToast.show({
+            timeout: 2000,
+            icon: "fa-solid fa-circle-exclamation",
+            message: '暂时不能写入内容'
+        });
+        if (is_sm) {
+            left_menu_off();
+            is_left_menu_show = false;
+        }
+    })
+    //顶部菜单打开菜单
+    $('#top_menu_btn').click(function () {
+        if (is_left_menu_show) {
+            //默认时
+            left_menu_off();
+            is_left_menu_show = false;
+        } else {
+            //点击后
+            left_menu_open();
+            is_left_menu_show = true;
+        }
+    });
+    //设置页面列表内容
+    updateWeb();
+})
